61111639bf8e4b706749ab59c89522a8f0b1fa3f
vishwarambukwella <vishwarambukwella@gmail.com>
2026-06-08 12:36:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import phonenumbers
++94751621827 phonenumbers
 from test import selvi
 
 from phonenumbers import geocoder
-ch_number = phonenumbers.parse(selvi,"ch")
+ch_number = +94751621827.parse(selvi,"ch")
 print(geocoder.description_for_number(ch_number,"en"))
 
 from phonenumbers import carrier
-service_number = phonenumbers.parse(selvi ,"RO")
+service_number = +94751621827.parse(selvi ,"RO")
 print(carrier.name_for_number(ch_number ,"en"))
